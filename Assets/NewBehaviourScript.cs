e5cacb59b87759e2533efc29c17dad8d28a3d618
Miszimaru <m-pl-k@wp.pl>
2017-12-02 10:37:03 +0100

    Dodanie planszy

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ruch : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float szybkosc = Time.deltaTime * 15; // szybkosc poruszania
+        if (Input.GetKey("up"))
+            transform.Translate(0, 0, szybkosc); //poruszanie sie do pszodu
+        if (Input.GetKey("down"))
+            transform.Translate(0, 0, -szybkosc); //poruszanie sie do tylu
+        if (Input.GetKey("left"))
+            transform.Translate(-szybkosc, 0, 0); //poruszanie sie w lewo
+        if (Input.GetKey("right"))
+            transform.Translate(szybkosc, 0, 0); //poruszanie sie w prawo
+
+    }
+
+}
