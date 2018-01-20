f3442640680de2dc27c2901d6208c9c295e060dd
Miszimaru <m-pl-k@wp.pl>
2018-01-20 12:21:03 +0100

    no message

---
+++ Assets/scripts/RandomCountry.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RandomCountry : MonoBehaviour
+{
+    public int interval = 3;
+    private DateTime lastTime;
+    public List<string> countries;
+    private System.Random random;
+    public TextMesh textMesh;
+
+	void Start ()
+    {
+        lastTime = DateTime.Now;
+        random = new System.Random();
+	}
+	
+	void Update ()
+    {
+        string country;
+		if (DateTime.Now > lastTime.AddSeconds(interval))
+        {
+            lastTime = DateTime.Now;
+            country = countries[random.Next(countries.Count)];
+            textMesh.text = country;
+        }
+	}
+}
