b55ebc5e5b3cfaece0b00cbe735400c8bab1d637
Miszimaru <m-pl-k@wp.pl>
2018-01-20 12:17:38 +0100

    no message

--- Assets/scripts/Tp.cs
+++ Assets/scripts/Tp.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class Tp : MonoBehaviour 
 {
 	public GameObject destination;
+    public string transportTag = "Medicine";
 	void OnTriggerEnter(Collider collider)
 	{
-		if (collider.CompareTag ("Player") || collider.gameObject.layer == 1) 
+		if (collider.CompareTag (transportTag) || collider.gameObject.layer == 1) 
 		{
 			Vector3 targetPosition = destination.transform.position;
 			targetPosition += destination.transform.forward * 2;
 			collider.transform.position = targetPosition;
-			collider.transform.Rotate (collider.transform.up, 180);
+			collider.transform.Rotate (collider.transform.up, 90);
 		}
 	}
 }
